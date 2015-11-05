988e9c68803964ea5e085de1c0ec2c5a3d90a4d7
Yannis Kalligeros <yak@4rt6.com>
2015-11-05 19:59:01 +0200

    .

---
+++ piface/flashled_2.py
@@ -0,0 +1,20 @@
+# source: http://www.piface.org.uk/assets/docs/PiFace-Digital2_getting-started.pdf
+# remember to do: apt-get install python3-pifacedigitalio
+
+import pifacedigitalio
+from time import sleep
+
+pfd = pifacedigitalio.PiFaceDigital()
+i = 0
+try:
+    while(True):
+        # invert LED state
+        pfd.leds[7].toggle()
+
+        # sleep like KIT
+        i += 1
+        slp = abs(50 - (i % 100))
+        sleep(slp / 100.0)
+finally:
+    # be polite and leave the LED off when exiting
+    pfd.output_pins[0].value = 0
